a89cf522bb07181163349e9bff791ee70921a663
Higor Gabriel <higorgabriel443@gmail.com>
2023-12-16 11:46:10 -0300

    alteracao
    alteração no css

---
+++ js/estante.js
@@ -0,0 +1,10 @@
+// $('.open').click(function(e){
+//     alert("is working!");
+// });
+
+function show(){
+    if($('.ocultar').val()=="esconde")
+    jQuery('.mostrarOuEsconder').hide();
+    if($('.mostra').val()=="mostra")
+    jQuery('.mostrarOuEsconder').show();
+}
